5c1ae87de3cd481442822aadd193c25599048b8f
Ethan Harrington <ethan.harrington@scsstudent.org>
2026-02-03 14:53:09 -0800

    feat: Finished designing HTML

--- website/js/script.js
+++ website/js/script.js
@@ -1,21 +1,24 @@
-const url =
-  "https://student-api-proxy.onrender.com/api/film-show-ratings.p.rapidapi.com/item/?id=tt0468569";
-const options = {
-  method: "GET",
-  headers: {
-    "X-API-Key":
-      "5257b26284411cf34833a4ab1c78fe6633489e5cdf9ec4d5d425101d7ad3bdeb",
-  },
-};
+const shawshankbutton = document.querySelector("#shawshank-btn");
+const godfatherbutton = document.querySelector("#godfather-btn");
 
-fetch(url, options)
-  .then((response) =>
-    response.json().then((result) => {
-      console.log(result.data); // Your API data
-      console.log(`Cost: $${result.meta.cost}`);
-      console.log(`Remaining: $${result.meta.remaining_budget}`);
-    }),
-  )
-  .catch((error) => {
-    console.log(error);
-  });
+// const url =
+//   "https://student-api-proxy.onrender.com/api/film-show-ratings.p.rapidapi.com/item/?id=tt0780504";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-API-Key":
+//       "5257b26284411cf34833a4ab1c78fe6633489e5cdf9ec4d5d425101d7ad3bdeb",
+//   },
+// };
+
+// fetch(url, options)
+//   .then((response) =>
+//     response.json().then((result) => {
+//       console.log(result.data); // Your API data
+//       console.log(`Cost: $${result.meta.cost}`);
+//       console.log(`Remaining: $${result.meta.remaining_budget}`);
+//     }),
+//   )
+//   .catch((error) => {
+//     console.log(error);
+//   });
